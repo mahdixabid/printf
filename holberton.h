42f06ebf0249f09cc909ebf0a6ecf0126cdfed44
omaryahia4 <omar.yahia4444@gmail.Com>
2021-03-16 19:42:40 +0000

    header file

---
+++ holberton.h
@@ -0,0 +1,28 @@
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
+#include <limits.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* struct op - flag / function object
+* @c: flag
+* @f: function
+*/
+
+typedef struct op
+{
+	char c;
+	int (*f)(va_list);
+} op_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_c(va_list ap);
+int get_func(char s, va_list ap);
+int print_s(va_list ap);
+int print_d(va_list n);
+int print_i(int n);
+
+#endif
