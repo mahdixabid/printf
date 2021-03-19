132f37a39c10cd75ccad8a69dff37af678722883
mahdixabid <mahdiabid158@gmail.com>
2021-03-19 10:34:02 +0100

    update

--- print_string.c
+++ print_string.c
@@ -47,3 +47,28 @@ int print_percent(va_list arg)
 	_putchar('%');
 	return (1);
 }
+
+#include "holberton.h"
+
+/**
+*print_rot13 - prints the rot13'ed string
+*@str: char to be written
+*@args: argument
+*
+*Return: int
+*/
+int rot13_format(char **str, va_list args)
+{
+	char *s;
+	char *rot;
+	int i;
+
+	s = va_arg(args, char *);
+	rot = rot13(s);
+	for (i = 0; rot[i]; i++)
+	{
+		**str = rot[i];
+		(*str)++;
+	}
+	return (0);
+}
