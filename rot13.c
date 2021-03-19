c2f81995ce6c768776d272075d99dc31f08995c7
mahdixabid <mahdiabid158@gmail.com>
2021-03-19 10:12:30 +0100

    rot13

---
+++ rot13.c
@@ -0,0 +1,25 @@
+
+#include "holberton.h"
+
+/**
+*rot13_format - prints the rot13'ed string
+*@str: char to be written
+*@args: argument
+*
+*Return: int
+*/
+int rot13_format(char **str, va_list args)
+{
+	char *s;
+	char *rot;
+	int i;
+
+	s = va_arg(args, char *);
+	rot = rot13(s);
+	for (i = 0; rot[i]; i++)
+	{
+		**str = rot[i];
+		(*str)++;
+	}
+	return (0);
+}
